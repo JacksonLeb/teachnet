0efab09651816315d79c648e8023203be2da6dda
Jackson Leb <jacksonleb@Jacksons-MacBook-Pro.local>
2022-12-25 16:14:37 -0500

    added multiple class and data collection

--- src/components/ClassLabelSubmit.jsx
+++ src/components/ClassLabelSubmit.jsx
@@ -1,18 +1,21 @@
-import React, {useState} from 'react'
+import React, {useState, useRef} from 'react'
 import ImageCap from './ImageCap'
 
-function ClassLabelSubmit(props) {
-    const [classLabel, setClassLabel] = useState("")
+function ClassLabelSubmit() {
+    const [classLabel, setClassLabel] = useState('')
     const [classes, setClasses] = useState([])
+    const textRef = useRef();
 
     const handleChange = (e) => {
+        e.preventDefault();
         setClassLabel(e.target.value);
     }
 
     const handleSubmit = () => {
-        setClasses(classes => [...classes, classLabel])
-        console.log(classes)
-        setClassLabel("");
+        setClasses(classes => [...classes, classLabel]);
+        setClassLabel('');
+        console.log(textRef.current.val);
+        textRef.current.value = '';
     }
 
   return (
@@ -20,24 +23,34 @@ function ClassLabelSubmit(props) {
         <form>
             <label>Class Name: </label>
             <div className='flex'>
-                <input class="placeholder:italic placeholder:text-slate-400 block bg-white w-1/4 border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" 
-                placeholder="Class Name..." 
-                type="text" 
-                name="search"
-                onChange={handleChange}/>
-
+                <input 
+                    class="placeholder:italic placeholder:text-slate-400 block bg-white w-1/4 border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" 
+                    placeholder="Class Name..." 
+                    type="text" 
+                    ref={textRef}
+                    onChange={handleChange}
+                />
                 <button 
-                class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mx-4"
-                onMouseDown={handleSubmit}>
-                    Submit Class
+                    class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mx-4"
+                    onClick={handleSubmit}
+                    type="button">
+                        Submit Class
                 </button>
             </div>
         </form>
         <div>
-            {classes.map((e) => (
-                <ImageCap classLabel={e} class="rounded-lg" key={e} src={e} alt={e}/>
-            ))}
+            <div class="flex">
+                {classes.map((e) => (
+                    <ImageCap classLabel={e} class="rounded-lg" key={e} src={e} alt={e}/>
+                ))}
+            </div>
         </div>
+        <button
+            class="bg-pink-500 hover:bg-pink-700 text-white font-bold py-2 px-4 rounded-full mx-4"
+            type="button"
+        >
+            Load Model
+        </button>
     </div>
   )
 }
